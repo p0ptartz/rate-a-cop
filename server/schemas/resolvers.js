346422dc326db12c754063e1639ab51df3f5c26e
neffej <118024182+neffej@users.noreply.github.com>
2023-03-10 13:30:13 -0500

    added query and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,43 @@
+const { Department, Feedback, Location, User } = require('../models');
+
+const resolvers = {
+    Query: {
+        feedbacks: async() => {
+            return Feedback.find().sort({ createdAt: -1 });
+        },
+
+        feedback: async() => {
+            return Feedback.findOne({ _id: feedbackId });
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this username');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, { username, password })=> {
+            const user = await User.create({ username, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        // addFeedback: async (parent, { review, rating }, context) => {
+        //     if (context.user)
+        // }
+    }
+}
+
+module.exports = resolvers;
