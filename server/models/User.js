cc5e2dfc9a2fca0b6eaceb3b93048a33a2d5df1c
neffej <118024182+neffej@users.noreply.github.com>
2023-03-11 10:29:55 -0500

    added bcrypt to User.js model

--- server/models/User.js
+++ server/models/User.js
@@ -19,6 +19,21 @@ const usersSchema = new Schema({
 
 });
 
+//Save password
+usersSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+//Compare password
+usersSchema.methods.isCorrectPassword = async function (password) {
+await bcrypt.compare(password, this.password);
+};
+
 const Users = model('Users', usersSchema);
 
 module.exports = Users
