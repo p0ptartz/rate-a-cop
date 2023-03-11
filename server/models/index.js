4e6cc9ea27743752aab35a7643869d1f652b5228
p0ptartz <ericwm420@gmail.com>
2023-03-10 20:47:17 -0500

    required models

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Department = require("./Department")
+const Feedback = require("./Feedback")
+const Location = require("./Location")
+const Officer = require("./Officer")
+const User = require("./User")
+
+module.exports = { Department, Feedback, Location, Officer, User }
