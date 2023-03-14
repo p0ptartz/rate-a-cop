4088087d4d2c15220ea68ecbe9ce89775f4d024b
Melona-Png <madisont.woodward@gmail.com>
2023-03-14 11:45:10 -0400

    refactored auth via passport

--- server/middleware.js
+++ server/middleware.js
@@ -1,4 +1,5 @@
 const authenticate = (resolve, parent, args, context, info) => {
+  // add this conditional to any resolver that requires authentication
     if (!context.user) {
       throw new Error('You must be logged in to access this resource');
     }
