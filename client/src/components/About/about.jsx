c9979d4412df77efa6b1f0ae1a8b69e6ecf8ee89
neffej <118024182+neffej@users.noreply.github.com>
2023-03-15 20:29:38 -0400

    changed class to className in about.jsx

--- client/src/components/About/about.jsx
+++ client/src/components/About/about.jsx
@@ -7,34 +7,34 @@ import bitmap2 from "./bitmap2.png"
 function About (){
     return (
  <div>
-    <div class = "flex flex-container">
+    <div className = "flex flex-container">
         {/* //INCOGNITO */}
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             
-                <img class="rounded-t-lg icon-size" src={bitmap} alt="image" />
+                <img className="rounded-t-lg icon-size" src={bitmap} alt="image" />
         
-            <div class="p-5">
+            <div className="p-5">
                 
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Incognito</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Your entries will always be incognito.</p>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Incognito</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Your entries will always be incognito.</p>
             </div>
         </div>
         {/* //ABOUT */}
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 about-card">
-            <div class="p-5">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 about-card">
+            <div className="p-5">
                 
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">About</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">TEXTY BOI</p>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">About</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">TEXTY BOI</p>
             </div>
         </div>
         {/* //INSIGHTS */}
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             
-                <img class="rounded-t-lg icon-size2" src={bitmap2} alt="image" />
-            <div class="p-5">
+                <img className="rounded-t-lg icon-size2" src={bitmap2} alt="image" />
+            <div className="p-5">
                 
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Insights</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Help promote transparency.</p>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Insights</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Help promote transparency.</p>
                     
             </div>
         </div>
